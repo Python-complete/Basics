02891eb8195b5c77c36e9d7614415c1414d49fbf
Arpan Basu <arpanbasu2002@gmail.com>
2023-09-03 21:42:20 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("Hello") # Print the string given
+n = int(input("Enter a number \n")) # Enter a number from the user and print the number
+print(n)
+
+# Single line Comment 
+''' Multi-Line
+    Comment '''
+
+x = 5
+x = x + 15
+print(x)
+x = 5
+x += 15
+print(x)
